783659aa731cbed31aaf0bd44edc672621bea3b9
dejan <dekac9@yahoo.com>
2021-09-26 11:56:26 +0200

    Prost zadatak funkcije umesto klasa

---
+++ 13 funkcije vezba prosto/vezba/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+
+function MyInfo(){
+return(
+<div>
+<h1>Dejan Mitrovic</h1>
+<p>Opasno uporan tip</p>
+<ul>
+  <li>Prvo mesto za letovanje Hrvatska</li>
+  <li>Drugo mesto za letovanje Bijela</li>
+  <li>Trece mesto za letovanje Grcka</li>
+</ul>
+</div>
+)
+}
+
+export default MyInfo

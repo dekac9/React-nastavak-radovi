156dfa0b2b8ed290a28f5027d04c10923e483251
dejan <dekac9@yahoo.com>
2021-09-26 12:13:07 +0200

    zavrseno upoznavanjesa fjama, osnovni primer

--- 13 funkcije vezba prosto/vezba/src/Components/MyInfo.js
+++ 13 funkcije vezba prosto/vezba/src/Components/MyInfo.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import "./App.css"
+import "../MyInfo.css"
 
 
 
 function MyInfo(){
 return(
 <div>
-<h1 className="ime">Dejan Mitrovic</h1>
+<h1 className="ime">Dejan D. Mitrovic</h1>
 <p>Opasno uporan tip</p>
 <ul>
-  <li>Prvo mesto za letovanje Hrvatska</li>
+  <li>Prvo mesto zza letovanje Hrvatskaa</li>
   <li>Drugo mesto za letovanje Bijela</li>
   <li>Trece mesto za letovanje Grcka</li>
 </ul>

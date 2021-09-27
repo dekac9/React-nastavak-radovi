b509856cb4b103cef5f8a93150c29287fea760c1
dejan <dekac9@yahoo.com>
2021-09-27 10:59:50 +0200

    sam odradio zadatak pozdravne poruke jsx + js

--- 13 funkcije vezba prosto/jsx_js/jsx/src/App.js
+++ 13 funkcije vezba prosto/jsx_js/jsx/src/App.js
@@ -2,9 +2,21 @@
 import './App.css';
 
 function App() {
+  const ime="Dejan";
+  const prezime = "Mitrovic"
+  const vreme = parseInt((new Date()).getHours());
+  var pozdrav="";
+
+if(vreme<12){
+pozdrav="Dobro jutro"
+
+}else{
+pozdrav="Dobro ti sve ostalo"
+}
+
   return (
-    <div>
-      
+    <div className="pozdrav">
+     {pozdrav} {ime}e {prezime}u, sada je {vreme} sati 
     </div>
   );
 }

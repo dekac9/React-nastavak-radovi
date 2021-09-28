86e1b48b5104ff8f02f068576128e4f0bb2f3bca
dejan <dekac9@yahoo.com>
2021-09-28 12:06:44 +0200

    provezbao primer sa propsima, props kao objekat

--- 13 funkcije vezba prosto/kontakt-kartice-propsi/rad/src/App.js
+++ 13 funkcije vezba prosto/kontakt-kartice-propsi/rad/src/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
+import React from "react";
 
-import './App.css';
+import "./App.css";
+import Kartica from "./Kartica";
 
 
 
 function App() {
   return (
     <div className="container">
-      <div className="kartica">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/a/a3/June_odd-eyed-cat.jpg"></img>
-      <h3>Cudnooka macka</h3>
-      <p>Telefon: Neki</p>
-      <p>Adresa: Negde</p>
-      </div>
-      <div className="kartica">
-      <img src="https://upload.wikimedia.org/wikipedia/commons/2/27/Black_white_cats.jpg"></img>
-      <h3>Dve macke</h3>
-      <p>Telefon: Neki DRUGI</p>
-      <p>Adresa: Negde DRUGDE</p>
-      </div>
-      
+      <Kartica kontakt={{ime:"Cudnooka macka",
+  urlSlika:"https://upload.wikimedia.org/wikipedia/commons/a/a3/June_odd-eyed-cat.jpg",
+  telefon:"Telefon: Neki",
+  adresa:"Adresa: Negde"}}
+        
+      >
+      </Kartica>
+      <Kartica kontakt={{ime:"Dve macke",
+  urlSlika:"https://upload.wikimedia.org/wikipedia/commons/2/27/Black_white_cats.jpg",
+  telefon:"Telefon: Neki DRUGI",
+  adresa:"Adresa: Negde DRUGDE"}}
+        
+      >
+      </Kartica>
     </div>
   );
 }
--- 13 funkcije vezba prosto/kontakt-kartice-propsi/rad/src/Kartica.js
+++ 13 funkcije vezba prosto/kontakt-kartice-propsi/rad/src/Kartica.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React from "react";
 
-function Kartica(props){
-
-return (props)
+function Kartica(props) {
+  console.log(props)
+  return (
+    <div className="kartica">
+      <img src={props.kontakt.urlSlika}></img>
+      <h3>{props.kontakt.ime}</h3>
+      <p>{props.kontakt.telefon}</p>
+      <p>{props.kontakt.adresa}</p>
+    </div>
+  );
 }
 
-export default Kartica
+export default Kartica;

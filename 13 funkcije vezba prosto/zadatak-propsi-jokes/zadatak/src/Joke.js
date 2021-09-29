ac87668563598456e61be40afec22781ad12811b
dejan <dekac9@yahoo.com>
2021-09-29 10:40:13 +0200

    prepravljao , dodavao logiku ternarnim operatorom

--- 13 funkcije vezba prosto/zadatak-propsi-jokes/zadatak/src/Joke.js
+++ 13 funkcije vezba prosto/zadatak-propsi-jokes/zadatak/src/Joke.js
@@ -3,11 +3,13 @@ import React from "react";
 
 
 function Joke(props) {
-
+  let temp = props.obj.pitanje;
+  let karakt = temp?"block" : "none";
+  let boja = temp?"black":"dodgerblue"
   return (
     <div className="kartica">
-      <p style={{display:!props.obj.pitanje && "none"}}>Pitanje: {props.obj.pitanje}</p>
-      <p>Odgovor: {props.obj.odgovor}</p>
+      <p style={{display:karakt}}>Pitanje: {props.obj.pitanje}</p>
+      <p style={{color:boja}}>Odgovor: {props.obj.odgovor}</p>
     </div>
   );
 }

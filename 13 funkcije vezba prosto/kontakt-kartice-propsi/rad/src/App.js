cb9961fc2566ac50f2db74070117c77a212c2ed1
dejan <dekac9@yahoo.com>
2021-09-29 22:51:18 +0200

    vraceno na staro

--- 13 funkcije vezba prosto/kontakt-kartice-propsi/rad/src/App.js
+++ 13 funkcije vezba prosto/kontakt-kartice-propsi/rad/src/App.js
@@ -8,7 +8,20 @@ import Kartica from "./Kartica";
 function App() {
   return (
     <div className="container">
-      
+      <Kartica kontakt={{ime:"Cudnooka macka",
+  urlSlika:"https://upload.wikimedia.org/wikipedia/commons/a/a3/June_odd-eyed-cat.jpg",
+  telefon:"Telefon: Neki",
+  adresa:"Adresa: Negde"}}
+        
+      >
+      </Kartica>
+      <Kartica kontakt={{ime:"Dve macke",
+  urlSlika:"https://upload.wikimedia.org/wikipedia/commons/2/27/Black_white_cats.jpg",
+  telefon:"Telefon: Neki DRUGI",
+  adresa:"Adresa: Negde DRUGDE"}}
+        
+      >
+      </Kartica>
     </div>
   );
 }

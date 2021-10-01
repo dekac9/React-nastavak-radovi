2959415e664a55b878331d9fa14710fb7f40e4f7
dejan <dekac9@yahoo.com>
2021-10-01 06:39:38 +0200

    dodat productData

---
+++ 14 primercic komp-props-dugm/vezba/src/productData.js
@@ -0,0 +1,27 @@
+const proizvod=[
+  {
+    id:1,
+    name:"olovka",
+    price:5,
+    desc:"Za pisanje"
+  },
+  {
+    id:2,
+    name:"gumica",
+    price:2,
+    desc:"Za brisanje"
+  },
+  {
+    id:3,
+    name:"sveska",
+    price:10,
+    desc:"Za zapisivanje"
+  },
+  {
+    id:4,
+    name:"knjiga",
+    price:20,
+    desc:"Za citanje"
+  },
+]
+export default proizvod;

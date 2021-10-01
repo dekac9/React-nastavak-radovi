79adfacacc618a14dcd02f6fce919cebb0094fa3
dejan <dekac9@yahoo.com>
2021-10-01 06:47:22 +0200

    ubacio i dugmice u svaku komp na klik alert desc

--- 14 primercic komp-props-dugm/vezba/src/App.js
+++ 14 primercic komp-props-dugm/vezba/src/App.js
@@ -36,3 +36,21 @@ import proizvod from "./productData";
   // }
   
   // export default App;
+
+  function App() {
+    let ispis = proizvod.map((elem) => {
+      return (
+        <Products
+          key={elem.id}
+          proizvod={elem}
+          // id={elem.id}
+          // name={elem.name}
+          // price={elem.price}
+          // desc={elem.desc}
+        />
+      );
+    });
+    return <div>{ispis}</div>;
+  }
+  
+  export default App;
--- 14 primercic komp-props-dugm/vezba/src/Products.js
+++ 14 primercic komp-props-dugm/vezba/src/Products.js
@@ -1,4 +1,4 @@
-// import React from 'react';
+import React from 'react';
 
 // var css={
 //   border: "black solid 1px",
@@ -54,3 +54,40 @@
 // }
 
 // export default Products;
+
+
+//DODATI DUGMICI NA SVAKOJ KOMP< NA KLIK IZBACUJU DESC PROPERTI IZ OBJEKTA
+var css={
+  border: "black solid 1px",
+  color:"red",
+  textAlign:"center",
+  margin:"50px"
+  // marginBottom:"30px",
+  // marginLeft:"50px"
+}
+
+function Products(props){
+const opis=props.proizvod.desc;
+console.log(opis)
+function alarm(){
+  alert(opis)
+}
+
+return (<div style={css}>
+  
+  <p>ID:{props.id}</p>
+  <p>IME:{props.proizvod.name}</p>
+  <p>PRICE:{props.proizvod.price.toLocaleString('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  //DODATO ZA CENOVNI STRING
+})}</p>
+  <p>DESC:{props.proizvod.desc}</p>
+  <button onClick={alarm}>Klikni me za description</button>
+
+
+</div>
+)
+}
+
+export default Products;

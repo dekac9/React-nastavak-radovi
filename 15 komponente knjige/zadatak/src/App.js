9185210e2db8291bfe5ba42a231afac6d1b4f1ca
dejan <dekac9@yahoo.com>
2021-10-07 17:58:19 +0200

    objekat kao props, destruktuiranje

--- 15 komponente knjige/zadatak/src/App.js
+++ 15 komponente knjige/zadatak/src/App.js
@@ -3,39 +3,46 @@ import React from 'react'
 import Knjiga from "./Knjiga"
 import "./App.css";
 
+
+const knjige = [
+  {
+    slika:
+      "https://images-na.ssl-images-amazon.com/images/I/51MB5SD5GIL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+    ime: "Little Blue Truck's Halloween Board book – Lift the flap",
+    autor: "Alice Schertle",
+  },
+  {
+    slika:
+      "https://images-na.ssl-images-amazon.com/images/I/51MB5SD5GIL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+    ime: "Little Blue Truck's Halloween Board book – Lift the flap",
+    autor: "Alice Schertle",
+  },
+  {
+    slika:
+      "https://images-na.ssl-images-amazon.com/images/I/51MB5SD5GIL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+    ime: "Little Blue Truck's Halloween Board book – Lift the flap",
+    autor: "Alice Schertle",
+  },
+  {
+    slika:
+      "https://images-na.ssl-images-amazon.com/images/I/51MB5SD5GIL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+    ime: "Little Blue Truck's Halloween Board book – Lift the flap",
+    autor: "Alice Schertle",
+  },
+];
+
+
+
 const App = () => {
   return (
     <div className="ispis">
-      <Knjiga
-        slika="https://images-na.ssl-images-amazon.com/images/I/51MB5SD5GIL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg"
-        ime="Little Blue Truck's Halloween Board book – Lift the flap"
-        autor="Alice Schertle"
-      >Opis: jheifheiufheiwfhiefhefhweiufgieugf</Knjiga>
-      <Knjiga
-        slika="https://images-na.ssl-images-amazon.com/images/I/51MB5SD5GIL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg"
-        ime="Little Blue Truck's Halloween Board book – Lift the flap"
-        autor="Alice Schertle"
-      ></Knjiga>
-      <Knjiga
-        slika="https://images-na.ssl-images-amazon.com/images/I/51MB5SD5GIL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg"
-        ime="Little Blue Truck's Halloween Board book – Lift the flap"
-        autor="Alice Schertle"
-      ></Knjiga>
-      <Knjiga
-        slika="https://images-na.ssl-images-amazon.com/images/I/51MB5SD5GIL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg"
-        ime="Little Blue Truck's Halloween Board book – Lift the flap"
-        autor="Alice Schertle"
-      ></Knjiga>
-      <Knjiga
-        slika="https://images-na.ssl-images-amazon.com/images/I/51MB5SD5GIL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg"
-        ime="Little Blue Truck's Halloween Board book – Lift the flap"
-        autor="Alice Schertle"
-      ></Knjiga>
-      <Knjiga
-        slika="https://images-na.ssl-images-amazon.com/images/I/51MB5SD5GIL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg"
-        ime="Little Blue Truck's Halloween Board book – Lift the flap"
-        autor="Alice Schertle"
-      ></Knjiga>
+      {knjige.map((element) =>{
+        let {slika,ime,autor} = element
+      return (
+        <Knjiga slika={slika} ime={ime} autor={autor}></Knjiga>
+      )
+      
+      })}
     </div>
   );
 }
--- 15 komponente knjige/zadatak/src/Knjiga.js
+++ 15 komponente knjige/zadatak/src/Knjiga.js
@@ -1,20 +1,21 @@
 import React from 'react'
 import "./App.css"
 
-const Knjiga = ({slika,ime,autor,children}) => {
+const Knjiga = (props) => {
+let {slika,ime,autor} = props
+
   //let {slika,ime,autor}=props
   //prvi nacin, destruktuiras objekat props na propertije i njih nisanis
 
   //DRUGI NACIN
- // direktno u fju, umesto propsa, prihvaras destruktuiran objekat
+  // direktno u fju, umesto propsa, prihvaras destruktuiran objekat
   return (
     <div className="osnovno">
       <img src={slika} />
       <h2 style={{ color: "red" }}>{ime}</h2>
       <p>{autor}</p>
-      <p>{children}</p>
     </div>
   );
-}
+};
 
 export default Knjiga

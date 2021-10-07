aa8dea5d2f4ad81a54282636c6756a53dcff0149
dejan <dekac9@yahoo.com>
2021-10-07 10:48:57 +0200

    destruktuiranje propsa KORISNO

--- 15 komponente knjige/zadatak/src/Knjiga.js
+++ 15 komponente knjige/zadatak/src/Knjiga.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import "./App.css"
 
-const Knjiga = (props) => {
+const Knjiga = ({slika,ime,autor}) => {
+  //let {slika,ime,autor}=props
+  //prvi nacin, destruktuiras objekat props na propertije i njih nisanis
+
+  //DRUGI NACIN
+ // direktno u fju, umesto propsa, prihvaras destruktuiran objekat
   return (
     <div className="osnovno">
-      <img src={props.slika}/>
-      <h2 style={{color:"red"}}>{props.ime}</h2>
-      <p>{props.autor}</p>
+      <img src={slika}/>
+      <h2 style={{color:"red"}}>{ime}</h2>
+      <p>{autor}</p>
     </div>
   )
 }

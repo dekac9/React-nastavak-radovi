9557a6955577a25b4dcb2d5736c2ec7ed9fea2c1
dejan <dekac9@yahoo.com>
2021-10-18 15:33:41 +0200

    Zavresen useState

--- 18 Vezbanje Use state hook/use-state/src/Usestateobject.js
+++ 18 Vezbanje Use state hook/use-state/src/Usestateobject.js
@@ -1,23 +1,26 @@
-import  React, {useState} from 'react'
+import React, { useState } from "react";
 
 const Usestateobject = () => {
-  const[person,setPerson]=useState({
-    name:"Peter",
-    age:44,
-    message:"Pozdrav"
-  })
+  const [person, setPerson] = useState({
+    name: "Peter",
+    age: 44,
+    message: "Pozdrav",
+  });
 
-  const promeniPoruku = ()=>{
-setPerson(person.message = 'CAO')
-  }
+  const promeniPoruku = () => {
+
+    setPerson({...person, message: "CAO"});
+  };
   return (
     <>
       <h4>{person.name}</h4>
       <h4>{person.age}</h4>
-      <h4>{person.message +" "+ person.name}</h4>
-      <button type="button" onClick={promeniPoruku}></button>
+      <h4>{person.message + " " + person.name}</h4>
+      <button type="button" onClick={promeniPoruku}>
+        Promeni poruku
+      </button>
     </>
   );
-}
+};
 
-export default Usestateobject
+export default Usestateobject;
